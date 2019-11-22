fb037ccd575fe1f6960ec0224ecd6ede2eb100d7
Zdzisław Trzaska <zdzislaw.trzaska@altkomsoftware.pl>
2019-11-22 11:42:16 +0100

    Updates

---
+++ precache-manifest.c2a3b64ffc3b34f700d62cacda37e497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0e05d4482409dcbf643b069854dadd",
+    "url": "/react1/index.html"
+  },
+  {
+    "revision": "b869e62b77dea26f9881",
+    "url": "/react1/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0f6d6a554361c25f87b7",
+    "url": "/react1/static/js/2.a08bd458.chunk.js"
+  },
+  {
+    "revision": "b869e62b77dea26f9881",
+    "url": "/react1/static/js/main.38294082.chunk.js"
+  },
+  {
+    "revision": "509a00fd8886a5d16a84",
+    "url": "/react1/static/js/runtime-main.689f2ad4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react1/static/media/logo.25bf045c.svg"
+  }
+]);
